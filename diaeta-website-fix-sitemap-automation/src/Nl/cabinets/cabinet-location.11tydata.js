6084538f05b8f4142e6563306cb730d3ccf6af84
Pierre Abou-Zeid <pierre}diaeta.be>
2025-07-21 13:03:32 +0200

    Merge remote-tracking branch 'origin/master'

--- diaeta-website-fix-sitemap-automation/src/Nl/cabinets/cabinet-location.11tydata.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  layout: "layouts/base.njk",
-  lang: "nl",
-  title: "Locatie | Diaeta Brussel",
-  description: "Vind de locatie van Diaeta Brussel en maak een afspraak met diëtist Pierre Abou-Zeid.",
-}; 
